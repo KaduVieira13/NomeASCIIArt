dc6ab6abcc887302e5ef9cd6ad2d13bf95f7b46c
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-04-01 15:13:43 -0300

    Formatação de programa

--- Program.cs
+++ Program.cs
@@ -8,16 +8,16 @@ namespace NomeASCIIArt
         static void Main(string[] args)
         {
 
-          Console.WriteLine(" --- Exibindo seu nome em ASCIIArt --- \n");
-          Console.Write("Digite seu nome: ");
-          
-          string nomeUsuario = Console.ReadLine();
-          string exibir = FiggleFonts.Standard.Render(nomeUsuario);
-         
-          Console.WriteLine("Seu nome ficou assim: ");
-           Console.ForegroundColor = ConsoleColor.Red;
-           Console.WriteLine($"{exibir}");
-          Console.ResetColor();
+            Console.WriteLine(" --- Exibindo seu nome em ASCIIArt --- \n");
+            Console.Write("Digite seu nome: ");
+
+            string nomeUsuario = Console.ReadLine();
+            string exibir = FiggleFonts.Standard.Render(nomeUsuario);
+
+            Console.WriteLine("Seu nome ficou assim: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{exibir}");
+            Console.ResetColor();
 
         }
     }
