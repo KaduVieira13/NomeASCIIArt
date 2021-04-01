974f4ef496a6d0c261bd3c12f876e990d0aca03f
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-04-01 15:09:52 -0300

    Conclusão de programa ASCIIArt

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Figgle;
+
+namespace NomeASCIIArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          Console.WriteLine(" --- Exibindo seu nome em ASCIIArt --- \n");
+          Console.Write("Digite seu nome: ");
+          
+          string nomeUsuario = Console.ReadLine();
+          string exibir = FiggleFonts.Standard.Render(nomeUsuario);
+         
+          Console.WriteLine("Seu nome ficou assim: ");
+           Console.ForegroundColor = ConsoleColor.Red;
+           Console.WriteLine($"{exibir}");
+          Console.ResetColor();
+
+        }
+    }
+}
